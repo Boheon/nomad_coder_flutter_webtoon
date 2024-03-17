2586b165decdc7f34c441e1a65251431738ab221
Boheon Park <gjsdldi23@naver.com>
2024-03-17 19:18:42 +0900

    웹툰앱

---
+++ toonflix_real/lib/services/api_service.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:toonflix_real/models/webtoon_detail_model.dart';
+import 'package:toonflix_real/models/webtoon_episode_model.dart';
+import 'package:toonflix_real/models/webtoon_model.dart';
+
+class ApiService {
+  static const String baseUrl =
+      "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = "today";
+
+  static Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonsInstances = [];
+    final url = Uri.parse('$baseUrl/$today');
+    final response = await http.get(url); //async 비동기 완료까지 기다려야함
+    if (response.statusCode == 200) {
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        final instance = WebtoonModel.fromJson(webtoon);
+        webtoonsInstances.add(instance);
+      }
+      return webtoonsInstances;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse('$baseUrl/$id');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodeById(
+      String id) async {
+    List<WebtoonEpisodeModel> episodesInstance = [];
+    final url = Uri.parse('$baseUrl/$id/episodes');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodesInstance.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodesInstance;
+    }
+    throw Error();
+  }
+}
